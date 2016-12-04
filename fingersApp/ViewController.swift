477450521c69d060c21b01a4e16640b2497732ae
West Kraemer <WestKraemer@gmail.com>
2016-12-04 12:52:37 -0500

    added labels and button and text field to app

--- fingersApp/ViewController.swift
+++ fingersApp/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var fingersTextField: UITextField!
 
+    @IBAction func guess(_ sender: Any) {
+        print ("button pressed")
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
